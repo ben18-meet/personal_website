d3f233c1ae2061576a62cb98917155b1a2d369e9
ben18-meet <benlevi1905@gmail.com>
2018-01-28 19:02:59 +0200

    Database Works!

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
-import views
-#Making Database
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+from flask.ext.session import Session
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.debug=True
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+sess = Session()
 
 class User(db.Model):
 	#Create user variables
@@ -14,22 +17,34 @@ class User(db.Model):
 	pwd = db.Column(db.String(80), nullable = False)
 	highScore = db.Column(db.Integer, default = 0)
 
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+	user = User()
+	user.name = request.form['user_name']
+	user.email = request.form['user_email']
+	user.pwd = request.form['user_pwd']
+	db.session.add(user)
+	db.session.commit()
+	return render_template('game.html', user = user)
+
+#db.drop_all()
+#db.rollback()
+db.create_all()
 
 
 #website routing
-#@app.route('/')
-#def home: 
-	#return render_template('%s.html' % index.html)
+@app.route('/')
+def register(): 
+	return render_template('register.html')
 
-#@app.route('/game')
-#def render_static(game.html):
-#	return render_template('%s.html' % game.html)
-
-#@app.route('/about')
-#def render_static(about.html):
-#	return render_template('%s.html' % about.html)		
+@app.route('/game')
+def home():
+	return render_template('game.html')
 
 
 if __name__ == '__main__':
-	app.run()
+	app.secret_key = 'super secret key'
+	app.config['SESSION_TYPE'] = 'filesystem'
+	sess.init_app(app)
+	app.run(debug = True)
 
